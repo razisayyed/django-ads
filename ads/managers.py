b39404d0a6820ce13f034289032c934b7a70c046
Razi Alsayyed <razi.sayed@gmail.com>
2017-02-08 09:56:29 +0200

    change random ad retrieval logic

--- ads/managers.py
+++ ads/managers.py
@@ -1,3 +1,5 @@
+import random
+
 from ads.querysets import AdQuerySet
 from django.db.models import Manager, Count
 
@@ -12,19 +14,10 @@ class AdManager(Manager):
     def zone_ads(self, zone):
         return self.get_queryset().zone_ads(zone)
 
-    def current_ad(self, zone):
-        ads = list(self.zone_ads(zone).public().annotate(
-            impressions_count=Count('impressions')
-        ))
+    def random_ad(self, zone):
+        ads = []
+        for ad in self.zone_ads(zone).public():
+            ads += [ad] * ad.weight
         if not ads:
             return None
-        total_weight = 0
-        total_impressions = 0
-        for ad in ads:
-            total_weight += ad.weight
-            total_impressions += ad.impressions_count
-        for ad in ads:
-            allowed_impressions = (float(ad.weight) / float(total_weight)) * float(total_impressions)
-            if ad.impressions_count <= allowed_impressions:
-                return ad
-        return ads[0]
+        return random.choice(ads)
