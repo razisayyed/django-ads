4585e5c2493ae8689addc29777a73924998340e0
Razi Alsayyed <razi.sayed@gmail.com>
2018-07-26 12:45:32 +0300

    get zones choices sorted based on key

--- ads/utils.py
+++ ads/utils.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy
 
 
 def get_zones_choices():
-    for key in settings.ADS_ZONES:
+    for key in sorted(settings.ADS_ZONES):
         yield (key, gettext_lazy(settings.ADS_ZONES[key].get('name', 'Undefined')))
 
 
